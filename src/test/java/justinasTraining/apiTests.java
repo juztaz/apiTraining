dcabcba3f6ca5ff55017085957b022de104351cb
justinasz <justinasz@wix.com>
2019-02-19 17:24:13 +0200

    Initial

---
+++ src/test/java/justinasTraining/apiTests.java
@@ -0,0 +1,13 @@
+package justinasTraining;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class apiTests {
+	
+	@Test
+	public void apiTest() {
+		System.out.println("API test");
+	}
+
+}
